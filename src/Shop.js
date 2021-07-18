08799bca905f0b984c40f0d0e9875402668ad6a9
Shahosol <soltani.shaho@gmail.com>
2021-07-18 17:54:09 +0300

    improved visuals

--- src/Shop.js
+++ src/Shop.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import jeju from './jeju.jpg'; // with import
 
 function Shop() {
     useEffect(() => {
@@ -22,14 +23,34 @@ const divStyle = {
     marginLeft: '10vw'
 }
 
+const linkStyle = {
+    borderStyle: 'groove',
+    width: '16vw',
+    marginLeft: '32vw',
+    backgroundColor: 'gold'
+}
+
+const img = {
+    border: '2px solid red',
+    padding: '10px',
+    borderRadius: '25px'
+}
+
 // JSX STARTS FROM HERE
   return (
     <div className="App">
         {items.map(item => (
             <h1 key={item.id} style={divStyle}>
+                <img style={img} src={jeju} alt="" />
+                <br></br>
                 <Link to={`/shop/${item.id}`}>{item.name}</Link>
                 <br></br>
                 <a href="url">{item.website}</a>
+                <br></br>
+                <br></br>
+                <div style={linkStyle}>
+                <Link to={`/shop/${item.id}`}>More Details</Link>
+                </div>
                 
             </h1>
         ))}
